20b73cef80c98ae1c3423c1f660ec5bca0261fed
Nuria <timerkaevnuria@gmail.com>
2023-07-31 20:52:08 +0300

    changed imports of base and user

--- app/services/database/models/__init__.py
+++ app/services/database/models/__init__.py
@@ -1,4 +1,4 @@
-from app.services.database.models.base import Base
+from app.services.database.models.base import Base, BaseWithTime, BaseWithId
 from app.services.database.models.user import User
 
-__all__ = ("Base", "User")
+__all__ = ("Base", "BaseWithId", "BaseWithTime", "User")
