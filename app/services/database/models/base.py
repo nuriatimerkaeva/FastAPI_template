7587c1c7bd9a26443f5c162c48c1a9163a8a9133
Nuria <timerkaevnuria@gmail.com>
2023-07-31 20:48:46 +0300

    divided base into three classes

--- app/services/database/models/base.py
+++ app/services/database/models/base.py
@@ -1,24 +1,38 @@
+import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declared_attr, as_declarative
-from sqlalchemy import DateTime, Column
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import DateTime
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
 
 
 @as_declarative()
 class Base:
-    id: int
 
-    created_at = Column(
+    __abstract__: bool = True
+
+    @declared_attr
+    def __tablename__(self) -> str:
+        return type(self).__name__.lower()
+
+
+class BaseWithId:
+
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+
+
+class BaseWithTime:
+
+    created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         nullable=False,
         default=datetime.now,
     )
-    updated_at = Column(
+
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         nullable=False,
         default=datetime.now,
         onupdate=datetime.now,
     )
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
