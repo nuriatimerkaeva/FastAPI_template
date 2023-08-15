4f610eed1f3835ffe98938ed0d28a0223917b6d1
Nuria <timerkaevnuria@gmail.com>
2023-08-15 15:44:51 +0300

    changed UserUpdate

--- src/dto/user.py
+++ src/dto/user.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
+from src.common.types import _Role
 
 
 class UserDTO(BaseModel):
     id: int
-    role: str
+    role: _Role
 
     class Config:
         from_attributes = True
@@ -24,7 +25,12 @@ class UserCreate(UserDTO, UserWithEmail, UserPrivate):
     pass
 
 
-class UserUpdate(UserCreate):
+class UserUpdate(BaseModel):
+    role: Optional[_Role]
+    email: Optional[EmailStr]
+    username: Optional[str]
+    hashed_password: Optional[str]
+    is_active: Optional[bool]
     name: Optional[str]
     last_name: Optional[str]
-    birth_day: Optional[int]
+    birthday: Optional[int]
