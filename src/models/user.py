b7489b5c026dee0f02387d10cdc07969f631b515
Nuria <timerkaevnuria@gmail.com>
2023-08-15 15:45:43 +0300

    changed column with role

--- src/models/user.py
+++ src/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 
 from src.models.base import Base, BaseWithIdAndTime
+from src.common.types import _Role
 
 
 class User(Base, BaseWithIdAndTime):
@@ -10,8 +11,8 @@ class User(Base, BaseWithIdAndTime):
     name: Mapped[str] = mapped_column(index=True, nullable=True)
     last_name: Mapped[str] = mapped_column(index=True, nullable=True)
     username: Mapped[str]  = mapped_column(index=True, nullable=False, unique=True)
-    birth_day: Mapped[date] = mapped_column(nullable=True)
+    birthday: Mapped[date] = mapped_column(nullable=True)
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
-    role: Mapped[str] = mapped_column(default='User', nullable=False)
+    role: Mapped[_Role] = mapped_column(default='User', nullable=False)
