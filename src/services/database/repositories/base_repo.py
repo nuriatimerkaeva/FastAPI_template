ea9cdfc017348e8a760a2401d663adc66ea83377
Nuria <timerkaevnuria@gmail.com>
2023-08-16 21:04:23 +0300

    fixed base repository

--- src/services/database/repositories/base_repo.py
+++ src/services/database/repositories/base_repo.py
@@ -1,29 +1,30 @@
 from abc import ABC, abstractmethod
 from uuid import UUID
+from sqlalchemy import insert, select, update, delete
 from sqlalchemy.orm.exc import NoResultFound
-
+from src.services.database.database import async_session_maker
 
 
 class AbstractRepository(ABC):
 
     @abstractmethod
-    async def add_one():
+    async def add_one(self, data: dict) -> int:
         raise NotImplementedError
 
     @abstractmethod
-    async def get_one():
+    async def get_one(self, id: UUID):
         raise NotImplementedError
 
     @abstractmethod
-    async def get_all():
+    async def get_all(self):
         raise NotImplementedError
 
     @abstractmethod
-    async def update():
+    async def update(self, data: dict):
         raise NotImplementedError
 
     @abstractmethod
-    async def delete():
+    async def delete(self, data: dict):
         raise NotImplementedError
 
 
@@ -37,38 +38,41 @@ class SQLAlchemyRepository(AbstractRepository):
             await session.commit()
             return one_position.scalar_one()
 
-    async def get_one(self, id: UUID) -> str:
+    async def get_one(self, id: UUID):
         async with async_session_maker() as session:
             try:
-                stmt = select(self.model.id)
+                stmt = select(self.model).where(self.model.id == id)
                 one_position = await session.execute(stmt)
-                return one_position
+                return one_position.scalar()
             except NoResultFound:
                 return "Position was not found"
 
     async def get_all(self):
         async with async_session_maker() as session:
-            try:
-                stmt = select(self.model)
-                all_positions = await session.execute(stmt)
-                all_positions = [row[0].to_read_model() for row in all_positions.all()]
-                return all_positions
-            except NoResultFound:
-                return "Positions were not found"
+            stmt = select(self.model)
+            all_positions = await session.execute(stmt)
+            all_positions = [row[0].to_read_model() for row in all_positions.all()]
+            return all_positions
 
-    async def update(self, data: dict) -> str:
-        async with async_session_meker as session:
-            stmt = self.model.merge_with(**data)
+    async def update(self, data: dict):
+        async with async_session_maker() as session:
+            stmt = (
+                update(self.model)
+                .where(self.model.id == data['id'])
+                .values(**data)
+                .returning(self.model)
+            )
             updated_data = await session.execute(stmt)
             await session.commit()
-            return updated_data
+            return updated_data.scalar()
 
-    async def delete(self, data: dict) -> str:
-        async with async_session_maker as session:
-            try:
-                stmt = self.model.delete(data)
-                deleted_data = await session.execute(stmt)
-                await session.commit()
-                return deleted_data
-            except NoResultFound:
-                return "Data was not found"
+    async def delete(self, data: dict):
+        async with async_session_maker() as session:
+            stmt = (
+                delete(self.model)
+                .where(self.model.id == data['id'])
+                .returning(self.model)
+            )
+            deleted_data = await session.execute(stmt)
+            await session.commit()
+            return deleted_data.scalar()
