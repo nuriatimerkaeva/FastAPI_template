54117d02825fce6b7c220d02d8e871d0c283d400
Nuria <timerkaevnuria@gmail.com>
2023-08-29 21:49:09 +0300

    fixed database.py

--- src/services/database/database.py
+++ src/services/database/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import sessionmaker
+from src.services.database.settings import settings
 
-from src.services.database.config import POSTGRESQL_DB_URL
 
-engine = create_async_engine(POSTGRESQL_DB_URL, echo=True)
+engine = create_async_engine(settings.db_url(), echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_session(self) -> AsyncSession:
