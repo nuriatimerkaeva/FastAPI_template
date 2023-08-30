c62013252f0bbf53d7ce34b87bd24c36e0df0960
Nuria <timerkaevnuria@gmail.com>
2023-08-30 21:58:22 +0300

    cteated first migrations

---
+++ src/services/database/migrations/versions/64164c24f8a4_created_tables.py
@@ -0,0 +1,24 @@
+"""created tables
+
+Revision ID: 64164c24f8a4
+Revises: 
+Create Date: 2023-08-30 20:48:19.429558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '64164c24f8a4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
