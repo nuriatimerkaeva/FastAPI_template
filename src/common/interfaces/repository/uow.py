f4bbdf09a5c6c8a683a556049300deb8b69e8ec9
Nuria <timerkaevnuria@gmail.com>
2023-09-02 17:26:51 +0300

    added unit of work

---
+++ src/common/interfaces/repository/uow.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+
+class UnitOfWork:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
