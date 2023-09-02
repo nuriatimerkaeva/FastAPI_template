92c28bdd77015a5842a76e69deb016d2a9cada08
Nuria <timerkaevnuria@gmail.com>
2023-09-02 22:26:43 +0300

    fixed routers

--- src/api/user_routers.py
+++ src/api/user_routers.py
@@ -4,19 +4,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.services.database.repositories.user_repo import UserRepository
 from src.services.database.models.user import User
-from src.common.dto import UserDTO, UserCreate, UserUpdate
+from src.common.dto.user import UserDTO, UserCreate, UserUpdate
 
 router = APIRouter()
 
 
 @router.post("/create", response_model=UserCreate, status_code=200)
 async def create_new_user(new_user: UserCreate,
-                          user_crud: UserRepository = Depends(UserRepository),)
-    user = await user_crud.get_one(id=user.id)
+                          user_crud: UserRepository = Depends(UserRepository),
+                          ):
+    user = await user_crud.get_one(email=new_user.email)
     if user:
         raise HTTPException(
             status_code=400,
-            detail="The user with this username already exists.",
+            detail="The user with this email already exists.",
             )
     user = await user_crud.add_one(new_user)
     return user
@@ -24,7 +25,8 @@ async def create_new_user(new_user: UserCreate,
 
 @router.get("/me", response_model=UserDTO, status_code=200)
 async def get_user_by_id(user: UserDTO,
-                         user_crud: UserRepository = Depends(UserRepository))
+                         user_crud: UserRepository = Depends(UserRepository)
+                         ):
     user = await user_crud.get_one(id=user.id)
     if not user:
         raise HTTPException(
@@ -35,6 +37,14 @@ async def get_user_by_id(user: UserDTO,
 
 @router.put("/update", response_model=UserUpdate, status_code=200)
 async def update_user(user: UserUpdate,
-                      user_crud: UserRepository = Depends(UserRepository))
+                      user_crud: UserRepository = Depends(UserRepository)
+                      ):
     user = await user_crud.update(user)
+    return user
+
+@router.delete("/update", response_model=UserUpdate, status_code=200)
+async def update_user(user: UserDTO,
+                      user_crud: UserRepository = Depends(UserRepository)
+                      ):
+    user = await user_crud.delete(user)
     return user
