0aeacd2600ace89bd772d8fdc8d68f2afb1b7e10
Nuria <timerkaevnuria@gmail.com>
2023-09-24 17:48:08 +0300

    added new types

--- src/common/types.py
+++ src/common/types.py
@@ -4,3 +4,4 @@ from typing import TypeAlias, Literal, ParamSpec, TypeVar
 _Role: TypeAlias = Literal['user', 'admin', 'superuser']
 Param = ParamSpec('Param')
 EntryType = TypeVar('EntryType')
+Model = TypeVar("Model")
