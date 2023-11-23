9f9c1d8f5af8bfb0854ba6413b868c11f911c7bf
Nuria <timerkaevnuria@gmail.com>
2023-11-23 22:11:47 +0300

    fix routers

--- src/api/v1/dependencies.py
+++ src/api/v1/dependencies.py
@@ -1,2 +1,2 @@
-class UserRepositoryDependency:
+class UserRepositoryDependencyMarker:
     pass
--- src/api/v1/user_routers.py
+++ src/api/v1/user_routers.py
@@ -1,34 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException
-from typing import Any
+from typing import Any, Dict, Union
 
 from src.services.database.repositories.user_repo import UserRepository
 from src.services.database.models.user import User
 from src.common.dto.user import UserDTO, UserCreate, UserUpdate
-from src.api.v1.dependencies import UserRepositoryDependency
+from src.api.v1.dependencies import UserRepositoryDependencyMarker
 
 router = APIRouter()
 
 
 @router.post("/", response_model=User)
-async def create_user(new_user: UserCreate,
-                      user_crud: UserRepository = Depends(UserRepositoryDependency),
-                      ) -> Any
-    user = await user_crud.get_user_by_email(email=new_user.email)
+async def create_user(user_in: UserCreate, user_crud: UserRepository = Depends(UserRepositoryDependencyMarker)) -> UserDTO:
+    user = await user_crud.get_by_email(email=user_in.email)
     if user:
         raise HTTPException(
             status_code=400,
             detail="The user with this username already exists in the system.",
         )
-    user = await user_crud.create_user(obj_in=new_user)
+    user = await user_crud.create_user(obj_in=user_in)
     return user
 
 
-@router.get("/{email}/", response_model=User)
-async def get_user(email: str, user_crud: UserRepository = Depends(UserRepositoryDependency)):
-    user = await user_crud.get_user_by_email(email)
-    return user
+@router.put('/update/{user_id}')
+async def update_user(user_id: int, data: UserUpdate, current_user: UserDTO, user_crud: UserRepository = Depends(UserRepositoryDependencyMarker)) -> UserDTO:
+    return await user_crud.update_user(user_id=user_id, data=data)
 
-@router.get("/{id}/", response_model=User)
-async def get_user(id: int, user_crud: UserRepository = Depends(UserRepositoryDependency)):
-    user = await user_crud.get_user_by_id(id)
-    return user
+
+@router.delete('/delete/{user_id}')
+async def delete_user(user_id: int, current_user: UserDTO, user_crud: UserRepository = Depends(UserRepositoryDependencyMarker)) -> Dict[str, str]:
+    result = await user_crud.delete_user(user_id=user_id)
+    if result:
+        return {"message": "user successfully deleted"}
+    raise HTTPException(404, "user does not exists")
